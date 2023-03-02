dbcae60cc022e1d26f15c7c84f149734113382d6
Bianca Rosa <me@biancarosa.com.br>
2023-03-02 20:00:19 -0300

    interfaces

--- git/tag.go
+++ git/tag.go
@@ -2,8 +2,14 @@ package git
 
 import "fmt"
 
-func BuscaGitTag(tag, donoDoRepositorio, repo string) {
+type Buscador interface {
+	BuscaGitTag(tag, donoDoRepositorio, repo string)
+}
+
+type BuscadorGit struct {
+}
+
+func (b *BuscadorGit) BuscaGitTag(tag, donoDoRepositorio, repo string) {
 	fmt.Println("Buscando commits pertencentes a git tag...")
 	fmt.Printf("%s %s %s\n", tag, donoDoRepositorio, repo)
-
 }
